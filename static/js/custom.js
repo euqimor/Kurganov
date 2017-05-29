2f9ef9f7b828f2a7fb71d84d3f6af7faf7e055db
euqimor <romique@gmail.com>
2017-05-30 02:43:15 +0300

    half-baked one page site

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,18 +1,36 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    center:true,
-    margin:10,
-    stagePadding:50,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplaySpeed: 1000,
+        lazyLoad: true,
+        loop: true,
+        center: true,
+        margin: 0,
+        stagePadding: 50,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
         }
-    }
-})
+    })
+});
+
+$(function() {
+		$('.pop').on('click', function() {
+			$('.imagepreview').attr('src', $(this).find('img').attr('data-url'));
+			$('#imagemodal').modal('show');
+		});
+});
+
+window.addEventListener("hashchange", function () {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+});
